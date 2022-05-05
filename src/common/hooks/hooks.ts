02f3292d2d914594769559ccfd5a7a1b5dc3e97b
Tarasiukowski <tarasiuk.michal03@gmail.com>
2022-05-05 16:36:31 +0200

    create hooks file

---
+++ src/common/hooks/hooks.ts
@@ -0,0 +1,4 @@
+export { useMount } from './useMount.hook'
+export { useLocalStorage } from './useLocalStorage.hook'
+export { useEventListener } from './useEventListener.hook'
+export { useUpdate } from './useUpdate.hook'

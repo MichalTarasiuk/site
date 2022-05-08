53d0e3b4103d98ffdc3fe5cf5d982e3073225cef
Tarasiukowski <tarasiuk.michal03@gmail.com>
2022-05-08 17:02:34 +0200

    create fetcher utility

---
+++ src/common/utils/fetcher.utility.ts
@@ -0,0 +1,42 @@
+const fallbackMessgae = 'Something went wrong. Please try again later.'
+
+const setRequestInit = (requestInit: RequestInit) => {
+  const { body, method, ...restOfRequestInit } = requestInit
+
+  return {
+    ...restOfRequestInit,
+    ...(body && { body: JSON.stringify(body) }),
+    headers: {
+      ...restOfRequestInit.headers,
+      'Content-Type': 'application/json',
+    },
+  }
+}
+
+export const fetcher = async (input: RequestInfo, init?: RequestInit) => {
+  try {
+    const response = await fetch(input, init && setRequestInit(init))
+
+    if (response.ok) {
+      return response.json()
+    }
+
+    throw new ResponseError(response.statusText, response.status)
+  } catch (error) {
+    if (error instanceof ResponseError) {
+      throw error
+    }
+
+    throw new ResponseError(fallbackMessgae)
+  }
+}
+
+export class ResponseError extends Error {
+  constructor(message: string, public readonly status?: number) {
+    super(message)
+    // eslint-disable-next-line functional/no-this-expression -- required for extending error
+    this.name = 'ResponseError'
+    // eslint-disable-next-line functional/no-this-expression -- required for extending error
+    Object.setPrototypeOf(this, ResponseError.prototype)
+  }
+}

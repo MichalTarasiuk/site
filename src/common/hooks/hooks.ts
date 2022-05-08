2a3664de25b77196e8a1bcd1251198c2f83410fb
Tarasiukowski <tarasiuk.michal03@gmail.com>
2022-05-08 17:04:59 +0200

    create useUnMount hook

--- src/common/hooks/hooks.ts
+++ src/common/hooks/hooks.ts
@@ -1,3 +1,5 @@
 export { useMount } from './useMount.hook'
 export { useLocalStorage } from './useLocalStorage.hook'
 export { useUpdate } from './useUpdate.hook'
+export { useUnMount } from './useUnMount.hook'
+export { useTimeout } from './useTimeout.hook'

943fc40c2c1fde32338b4cedcbc274120f53d002
Tarasiukowski <tarasiuk.michal03@gmail.com>
2022-05-12 15:49:14 +0200

    add mdx config

--- next.config.js
+++ next.config.js
@@ -1,9 +1,23 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
+
+const withMDX = require('@next/mdx')({
+  extension: /\.mdx?$/,
+  options: {
+    remarkPlugins: [],
+    rehypePlugins: [],
+  },
+})
+
+const defaultConfig = {
+  pageExtensions: ['ts', 'tsx'],
+}
+
+const nextConfig = withMDX({
   reactStrictMode: true,
   devIndicators: {
     buildActivityPosition: 'bottom-left',
   },
-}
+  pageExtensions: [...defaultConfig.pageExtensions, 'mdx'],
+})
 
 module.exports = nextConfig

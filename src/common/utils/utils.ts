5dbb8d529941a597e03e71658ae5fed81a0fc488
Tarasiukowski <tarasiuk.michal03@gmail.com>
2022-05-26 22:18:13 +0200

    create reverseString utility

--- src/common/utils/utils.ts
+++ src/common/utils/utils.ts
@@ -2,6 +2,7 @@ export { fetcher } from './fetcher.utility'
 export { shallowEqual } from './shallowEqual.utility'
 export { createSafeContext } from './createSafeContext.utility'
 export { entries, fromEntries } from './entries.utility'
+export { filterObject } from './filterObject.utility'
 
 export const isFunction = (value: unknown): value is Function =>
   typeof value === 'function'
@@ -12,3 +13,8 @@ export const compact = <TValue>(arr: readonly TValue[]) => arr.filter(Boolean)
 
 export const uppercaseFirst = (value: string) =>
   value.slice(0, 1).toUpperCase() + value.slice(1)
+
+export const isEmpty = (object: object) => Object.keys(object).length === 0
+
+export const reverseString = (value: string, separator: string) =>
+  value.split(separator).reverse().join(separator)

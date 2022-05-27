fcf89a952c66ac5359dc4567f7f494397f39e1f8
Tarasiukowski <tarasiuk.michal03@gmail.com>
2022-05-27 11:48:26 +0200

    adjust useBeforeFirstPaint hook to srr

--- src/common/hooks/useBeforeFirstPaint.hook.ts
+++ src/common/hooks/useBeforeFirstPaint.hook.ts
@@ -1,8 +1,13 @@
-import { useLayoutEffect } from 'react'
+import { useLayoutEffect, useEffect } from 'react'
 
 import type { EffectCallback } from 'react'
 
+import { isClientEnvironment } from 'src/common/constants/constants'
+
+const useIsomorphicEffect = isClientEnvironment ? useLayoutEffect : useEffect
+
 export const useBeforeFirstPaint = (effect: EffectCallback) => {
   // eslint-disable-next-line react-hooks/exhaustive-deps -- should call only once
-  useLayoutEffect(effect, [])
+  console.log(useIsomorphicEffect)
+  useIsomorphicEffect(effect, [])
 }

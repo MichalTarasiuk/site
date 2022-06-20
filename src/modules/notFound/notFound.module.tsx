317f4614434681c20e81f0a484afe2059b29a9db
Tarasiukowski <tarasiuk.michal03@gmail.com>
2022-06-20 15:20:52 +0200

    create notfound module

---
+++ src/modules/notFound/notFound.module.tsx
@@ -0,0 +1,24 @@
+import { useState } from 'react'
+
+import Styles from './notFound.module.scss'
+
+import { usePaths, useBeforeFirstPaint } from 'src/common/hooks/hooks'
+import { DefaultLayout } from 'src/layouts/layouts'
+
+const NOT_FOUND_CODE = 404
+const FALLBACK_TITLE = '/path/not/found'
+
+export const NotFoundPage = () => {
+  const [title, setTitle] = useState(FALLBACK_TITLE)
+  const { pathname } = usePaths()
+
+  useBeforeFirstPaint(() => {
+    setTitle(pathname)
+  })
+
+  return (
+    <DefaultLayout title={title}>
+      <h1 className={Styles.heading}>{NOT_FOUND_CODE}</h1>
+    </DefaultLayout>
+  )
+}
